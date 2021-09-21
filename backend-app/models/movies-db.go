bfb80f927c69a49388ee168d4eec605ab1cfbf58
iftikharfadjar <fadjar.iftikhar@gmail.com>
2021-09-21 04:26:34 +0000

    add get all movies

--- backend-app/models/movies-db.go
+++ backend-app/models/movies-db.go
@@ -65,6 +65,67 @@ func (m *DBModel) Get(id int) (*Movie, error){
 	return &movie, nil
 }
 
-func (m *DBModel) All(id int) ([]*Movie, error){
-	return nil, nil
+func (m *DBModel) All() ([]*Movie, error){
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	
+	rows, err := m.DB.QueryContext(ctx, "SELECT * FROM movies ORDER BY title")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	
+	var movies []*Movie
+	
+
+	
+	for rows.Next(){
+		var movie Movie
+			err = rows.Scan(
+				&movie.ID,
+				&movie.Title,
+				&movie.Description,
+				&movie.Year,
+				&movie.ReleaseDate,
+				&movie.Rating,
+				&movie.Runtime,
+				&movie.MPAARating,
+				&movie.CreatedAt,
+				&movie.UpdatedAt,
+		)
+		
+		if err != nil {
+			return nil, err
+		}
+		
+		genrequery := "select mg.id, mg.movie_id, mg.genre_id, g.genre_name from movies_genres mg LEFT JOIN genres g ON g.id=mg.genre_id WHERE mg.movie_id=?"
+		
+		genreRows, _ := m.DB.QueryContext(ctx, genrequery,  movie.ID)
+		
+		// var genres []MovieGenre
+		genres := make(map[int]string)
+		for genreRows.Next(){
+			
+			var mg MovieGenre
+			err = genreRows.Scan(
+				&mg.ID,
+				&mg.MovieID,
+				&mg.GenreID,
+				&mg.Genre.GenreName,
+			)
+
+			if err != nil {
+				return nil, err
+			}
+
+		// 	// genres = append(genres, mg)
+			genres[mg.ID]=mg.Genre.GenreName
+			
+		}
+		genreRows.Close()
+
+		movie.MovieGenre = genres;
+		movies = append(movies, &movie)
+		}
+		return movies, nil
 }
