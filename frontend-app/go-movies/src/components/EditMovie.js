8dfd891e302677a12420e325201bd0c0e16a07c1
iftikharfadjar <fadjar.iftikhar@gmail.com>
2021-10-18 10:55:35 +0000

    make reusable component in form

--- frontend-app/go-movies/src/components/EditMovie.js
+++ frontend-app/go-movies/src/components/EditMovie.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import { Link} from 'react-router-dom';
 import './EditMovie.css'
+import {Input, TextArea, Select} from './form-components/form-components'
 
 export default class OneGenre extends Component {
 	state = {
@@ -8,6 +9,54 @@ export default class OneGenre extends Component {
 		isLoaded:false,
 		error: null,
 	};
+
+	constructor(props){
+		super(props);
+		this.state ={
+			movie : {
+				id:0,
+				title : "",
+				release_date : "",
+				runtime : "",
+				mpaa_rating :"",
+				rating : "",
+				description : "",
+			},
+			mpaaOption: [
+				{id:"G", value:"G"},
+				{id:"PG", value:"PG"},
+				{id:"PG13", value:"PG13"},
+				{id:"R", value:"R"},
+				{id:"NC17", value:"NC17"},
+				
+			],
+			isLoaded : false,
+			error: null,
+		}
+		
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+handleSubmit = (evt) => {
+	console.log("Form was submitted");
+	evt.preventDefault();
+}
+
+handleChange = (evt) => {
+	let value = evt.target.value;
+	let name = evt.target.name;
+	this.setState((prevState) => ({
+		movie : {
+			...prevState.movie,
+			[name] : value,
+		}
+	}))
+}
+	
+	componentDidMount(){
+		
+	}
 
 
 	render() {
@@ -17,51 +66,42 @@ export default class OneGenre extends Component {
 			<Fragment>
 				<h2>Add/Edit Movie</h2>	
 				<hr/>
-				<form method="post">
-					<div className="mb-3">
-						<label for="title" className="form-label">Title</label>
-						<input type="text" className="form-control" id="title" value={movie.title} />
-					</div>
+				<form onSubmit={this.handleSubmit}>
+					<input 
+						type="hidden"
+						name="id"
+						id="id"
+						value={movie.id}
+						onChange={this.handleChange}
+						/>
+	
+					<Input name="title" type="text" title="Title"  value={movie.title} handleChange={this.handleChange}/>
 					
-					<div className="mb-3">
-						<label for="release_date" className="form-label">Release Date</label>
-						<input type="text" className="form-control" id="release_date" value={movie.release_date} />
-					</div>
+		
 					
-					<div className="mb-3">
-						<label for="runtime" className="form-label">Runtime</label>
-						<input type="text" className="form-control" id="runtime" value={movie.runtime} />
-					</div>
+					<Input name="release_date" type="text" title="Release Date"  value={movie.release_date} handleChange={this.handleChange} placeholder="yyyy-mm-dd"/>
 					
-					<div className="mb-3">
-						<label for="mpaa_rating" className="form-label">MPAA Rating</label>
-						<select className="form-select" id="mpaa_rating" value={movie.mpaa_rating} >
-							<option className="form-select">Choose...</option>
-							<option className="form-select" value="G">G</option>
-							<option className="form-select" value="PG">PG</option>
-							<option className="form-select" value="PG13">PG13</option>
-							<option className="form-select" value="R">R</option>
-							<option className="form-select" value="NC17">NC17</option>
-						</select>
-					</div>
+
+					<Input name="runtime" type="text" title="Runtime"  value={movie.runtime} handleChange={this.handleChange}/>
 					
-					<div className="mb-3">
-						<label for="rating" className="form-label">Rating</label>
-						<input type="text" className="form-control" id="rating" value={movie.rating} />
-					</div>
+				
 					
-					<div className="mb-3">
-						<label for="description" className="form-label">Description</label>
-						<textarea className="form-control" id="description" name="description" rows="3">
-							{movie.description}
-						</textarea>
-					</div>
+					<Select name="mpaa_rating" title="MPAA Rating" value={movie.mpaa_rating} handleChange={this.handleChange} option={this.state.mpaaOption} ></Select>
 					
+					<Input name="rating" type="text" title="Rating"  value={movie.rating} handleChange={this.handleChange}/>
+					
+					
+					<TextArea name="description" handleChange={this.handleChange} value={movie.description} title="Description" />
 					
 					<hr/>
 					
 					<button className="btn btn-primary">Save</button>
 				</form>
+				
+				<div className="mt-3">
+					<pre>{JSON.stringify(this.state, null, 3)}</pre>
+				</div>
+				
 			</Fragment>
 			)
 	}
---
+++ frontend-app/go-movies/src/components/form-components/form-components.js
@@ -0,0 +1,56 @@
+const Input = (props) => {
+	return(
+		<div className="mb-3">
+			<label htmlFor={props.name} className="form-label">{props.title}</label>
+			<input type={props.type} 
+				className="form-control" 
+				id={props.name} 
+				name={props.name} 
+				value={props.value} 
+				onChange={props.handleChange} 
+				placeholder={props.placeholder}
+				/>
+		</div>
+	);
+}
+
+const TextArea = (props) => {
+	return(
+			<div className="mb-3">
+				<label htmlFor={props.name} className="form-label">{props.title}</label>
+				<textarea 
+					className="form-control" 
+					id={props.name} 
+					name={props.name} 
+					rows="3" 
+					onChange={props.handleChange} 
+					value={props.value} />
+			</div>
+	);
+}
+
+const Select = (props) => {
+	return(
+		<div className="mb-3">
+						<label htmlFor={props.name} className="form-label">{props.title}</label>
+						<select className="form-select" id={props.name} name={props.name} value={props.value} onChange={props.handleChange} >
+							<option className="form-select" value="">{props.placeholder}</option>
+							{props.option.map((opt) => {
+								return(
+									<option 
+										className="form-select"
+										key={opt.id}
+										value={opt.id}
+										label={opt.value}
+										>
+										{opt.value}
+									</option>
+								)
+							})}
+						</select>
+					</div>
+	)
+}
+
+export {TextArea, Input, Select};
+
